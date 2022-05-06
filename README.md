c8a1c4c5fce623567b298ecdb81505f3f21fe373
parsa.rahimi <parsa.rahimi.n@gmail.com>
2022-05-06 23:32:55 +0430

    Fixing the bug that causes the fonts not appearing in pdf files

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
-# genalog-extended
+# docgen (OCR)
 
 ## Installation
+The version of WeasyPrint must be 52.5. 
+we plan to completley remove `genalog` which causing some serious problems
 ```bash
 python3 -m pip install -r requirements.txt
 ```
 
 ## How to use:
 "dataset_generator.py" is the main file. 
+
+
 "texts/example.txt" is the input text. 
 "outputs/" is the output folder. 
 "templates/" is the folder to store templates in jinja2.
 "images/" is the images folder to use in the template.
-"fonts/" is the fonts folder.
+"fonts/" is the fonts folder. poupulate some fonts
 run "dataset_generator.py".
--- dataset_generator.py
+++ dataset_generator.py
@@ -35,7 +35,7 @@ def random_select(inp: List, count: int, logger=None):
     np.random.shuffle(inp)
     return inp[:count]
 
-np.random.seed(37)
+#np.random.seed(37)
 
 # Enum class for page sizes.
 class PageSize(enum.Enum):
@@ -54,10 +54,11 @@ text_path = "./texts/example.txt"
 text_base_path = "./texts"
 pdf_output_path = "./output"
 absolute_path = "file://" + os.path.dirname(os.path.abspath(__file__))
+absolute_path_wo = os.path.dirname(os.path.abspath(__file__))
 logger.info("Absolute path: {}".format(absolute_path))
-img_logo_base_path = "./img_logos/"
-img_sig_base_path = "./img_sigs/"
-font_base_path = "./fonts/"
+img_logo_base_path = "img_logos/"
+img_sig_base_path = "img_sigs/"
+font_base_path = "fonts/"
 
 all_fonts = glob(os.path.join(font_base_path, '/*.ttf'))
 
@@ -78,14 +79,15 @@ logger.info("Paragraph number is {}".format(len(paragraphs)))
 
 content_types = [ContentType.PARAGRAPH] * len(paragraphs)
 content = CompositeContent(paragraphs, content_types)
-default_generator = DocumentGenerator(template_path="./templates")
-doc_gen = default_generator.create_generator(content, ['letter.html.jinja'])
 
 # define style parameters.
 font_paths = random_select(glob(os.path.join(font_base_path, '*.ttf')), config.num_fonts)
 font_family = [ font_path.split('.')[0] for font_path in font_paths ] 
+font_paths = [ os.path.join("\"" + absolute_path, font_path + "\"") for font_path in font_paths ]
+
 img_logos = [ os.path.join(absolute_path, logo) for logo in random_select(glob(os.path.join(img_logo_base_path, '*.png')), config.num_logos) ] 
 img_signatures = [ os.path.join(absolute_path, sig) for sig in  random_select(glob(os.path.join(img_sig_base_path, '*emza*.png')), config.num_sigs) ]
+print("###"*10 + "\nSample font", font_paths[0])
 print("###"*10 + "\nSample logo path", img_logos[0]) 
 print("###"*10 + "\nSample sig path", img_signatures[0]) 
 
@@ -95,9 +97,6 @@ page_size = PageSize.A5
 gridWarper = GridWarper(random_area_ratio=0.13, logger=logger)
 
 new_style_combinations = {
-    "hyphenate": [False],
-    "font_size": ["11px"],
-    "font_family": [font_family],
     "text_align": ["right"],
     "language": ["fa"],
 
@@ -140,53 +139,67 @@ DEGRADATIONS = [
 ]
 
 # applying styles and degradation.
-default_generator.set_styles_to_generate(new_style_combinations)
-degrader = Degrader(DEGRADATIONS)
 
-for idx, doc in enumerate(doc_gen):
-    logger.info("Generating files for {}".format(idx))
 
-    base_output_path = os.path.join(pdf_output_path,str(idx))
-    os.makedirs(base_output_path, exist_ok=True)
 
-    file_name = os.path.join(base_output_path, "{}".format(str(idx))) 
-    deg_file_name = os.path.join(base_output_path, "{}".format(str(idx)))
+iter = 0
+while True: 
+    logger.info("iter no: {}".format(iter+1))
+    iter+=1 
 
-    pdf_name = file_name + ".pdf"
-    # Store Pdf with convert_from_path function.
+    default_generator = DocumentGenerator(template_path="./templates")
+    doc_gen = default_generator.create_generator(content, ['letter.html.jinja'])
+    default_generator.set_styles_to_generate(new_style_combinations)
+    degrader = Degrader(DEGRADATIONS)
     
-    doc.render_pdf(target=pdf_name, zoom=config.zoom)
-    # doc.render_png(target=png_name, resolution=300)
-    
-    # getting bounding boxes.
-    words, positions, cropbox = GetBoundingBoxes(pdf_name, print_output=False)
-    page_height = cropbox[3]
+    for idx, doc in enumerate(doc_gen):
+        logger.info("Generating files for {}".format(idx))
 
-    wbboxes = poses2bboxes(positions, page_height, config.zoom)    
+        base_output_path = os.path.join(pdf_output_path, f"{iter}-{idx}")
+        os.makedirs(base_output_path, exist_ok=True)
 
-    images = convert_from_path(pdf_name, dpi=config.dpi)
-    for i in range(len(images)):
-        png_name = file_name + "_" + str(i) + ".png"
-        deg_png_name = deg_file_name + "_" + str(i) + ".png"
+        file_name = os.path.join(base_output_path, "{}".format(str(idx))) 
+        deg_file_name = os.path.join(base_output_path, "{}".format(str(idx)))
 
-        # Save pages as images in the pdf.
-        images[i].save(png_name, 'PNG')
-        deg_image = degrader.apply_effects(cv.imread(png_name, cv.IMREAD_GRAYSCALE))
-
-        deg_image = deg_image.reshape((*deg_image.shape,1)) 
-        deg_image = np.concatenate((deg_image, deg_image, deg_image), axis=2)
-        # Random Warping
-        if config.warp.enabled: 
-            deg_image, wbboxes = gridWarper(deg_image, wbboxes)
+        pdf_name = file_name + ".pdf"
+        # Store Pdf with convert_from_path function.
         
+        doc.render_pdf(target=pdf_name, zoom=config.zoom)
+        # doc.render_png(target=png_name, resolution=300)
+        html_output = doc.render_html()
+        with open(os.path.join(base_output_path, 'string.html'), 'w') as file :
+            file.writelines(html_output)
+        
+        # getting bounding boxes.
+        words, positions, cropbox = GetBoundingBoxes(pdf_name, print_output=False)
+        page_height = cropbox[3]
 
-        deg_image = drawbboxes(deg_image, wbboxes, color=(207, 227, 226))
-        cv.imshow(winname, deg_image)
-        cv.waitKey(0)        
+        wbboxes = poses2bboxes(positions, page_height, config.zoom)    
 
-        cv.imwrite(deg_png_name, deg_image)
+        images = convert_from_path(pdf_name, dpi=config.dpi)
+        for i in range(len(images)):
+            png_name = file_name + "_" + str(i) + ".png"
+            deg_png_name = deg_file_name + "_" + str(i) + ".png"
 
+            # Save pages as images in the pdf.
+            images[i].save(png_name, 'PNG')
+            deg_image = degrader.apply_effects(cv.imread(png_name, cv.IMREAD_GRAYSCALE))
 
+            deg_image = deg_image.reshape((*deg_image.shape,1)) 
+            deg_image = np.concatenate((deg_image, deg_image, deg_image), axis=2)
+            # Random Warping
+            if config.warp.enabled: 
+                deg_image, wbboxes = gridWarper(deg_image, wbboxes)
+            
+
+            deg_image = drawbboxes(deg_image, wbboxes, color=(207, 227, 226))
+            cv.imshow(winname, deg_image)
+            cv.waitKey(3000)        
+
+            cv.imwrite(deg_png_name, deg_image)
+
+    if iter == 3: 
+        break
 
 # words, positions = GetBoundingBoxes(pdf_name, True)
 
